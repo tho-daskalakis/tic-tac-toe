16e9a788620598d46a891e5dde98c13b1f1ac96e
Thomas <tho.daskalakis@gmail.com>
2022-08-22 12:02:07 +0300

    Add win logic

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ const board = (function() {
     ];
 
     const getBoard = () => {
-        return this.boardArray;
-    }
+        return boardArray;
+    };
 
     const setBoard = (index, value) => {
 
         boardArray[index] = value;
 
         controller.updateDisplay();
-    }
+    };
     
     return {
         getBoard,
@@ -69,23 +69,83 @@ const controller = (function() {
     const updateDisplay = () => {
         
         for (let i = 0; i < 9; i++) {
-            
-            boardDivs[i].querySelector('p').textContent = 
-            
-            board.getBoard()[i];
+            boardDivs[i].querySelector('p').textContent = board.getBoard()[i];
         }
-    }
+    };
 
     const nextTurn = () => {
+        let winner = gameEnd.isWin();
+
+        if (winner !== undefined) {
+            console.log('Winner is ' + players[winner].getName());
+        }
+
+        if (gameEnd.isTie()) {
+            console.log('Game is a tie');
+        }
         
         turnNum++;
         
         currentPlayer = (currentPlayer + 1) % 2;
     };
 
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    }
+
     return {
         init,
         updateDisplay,
+        getCurrentPlayer,
+    };
+})();
+
+const gameEnd = (function() {
+
+    const isWin = () => {
+
+        const currentBoard = board.getBoard();
+
+        // Check rows
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[3*i] !== '' && 
+            (currentBoard[3*i] === currentBoard[3*i+1] && 
+            currentBoard[3*i+1] === currentBoard[3*i+2])) {                
+                return controller.getCurrentPlayer();
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[i] !== '' && 
+            (currentBoard[i] === currentBoard[i+3] && 
+            currentBoard[i+3] === currentBoard[i+6])) {
+                return controller.getCurrentPlayer();
+            }
+        }
+
+        // Check top-left to bot-right diagonal
+        if (currentBoard[0] !== '' && (
+        currentBoard[0] === currentBoard[4] && 
+        currentBoard[4] === currentBoard[8])) {
+            return controller.getCurrentPlayer();
+        }
+
+        // Check bot-left to top-right diagonal
+        if (currentBoard[2] !== '' && (
+            currentBoard[2] === currentBoard[4] && 
+            currentBoard[4] === currentBoard[6])) {
+                return controller.getCurrentPlayer();
+            }
+    };
+
+    const isTie = () => {
+
+    };
+
+    return {
+        isWin,
+        isTie,
     };
 })();
 
